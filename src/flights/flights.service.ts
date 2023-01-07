de7f2dc20beea05faf754982492ace7db60a6531
Raissa Hohenester <raissa.developer@gmail.com>
2023-01-07 12:05:19 +0100

    Combine sources

--- src/flights/flights.service.ts
+++ src/flights/flights.service.ts
@@ -3,21 +3,22 @@ import { HttpService } from '@nestjs/axios/dist';
 import { catchError, forkJoin, of, map } from 'rxjs';
 import { FlightsSlice } from './interfaces';
 
+
 @Injectable()
-export class FlightsService { //add config
+export class FlightsService {
+    flights: any; //add config
     constructor(private readonly httpService: HttpService) {}
     async findAll() {
         const urls = ['https://coding-challenge.powerus.de/flight/source1', 'https://coding-challenge.powerus.de/flight/source2'];
-
+        
         const flights = forkJoin(
             urls.map(source => 
                 this.httpService.get<FlightsSlice[]>(source).pipe(catchError(e => of(e)))
             )
-        )
-        .pipe(
-            map(x => x.flat())
-        )
-        .subscribe(data => {console.log(data)})
+        ).pipe(map((results: any[]) => {
+            const objs =  {flights: results[0].data["flights"].concat(results[1].data["flights"])};
+            return objs;
+        }));
 
         return flights;
     }
