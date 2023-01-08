43720c710a075100d4b3dd18a94391396557d71b
Raissa Hohenester <raissa.developer@gmail.com>
2023-01-08 12:39:09 +0100

    Filter duplicates solution

--- src/flights/flights.service.ts
+++ src/flights/flights.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios/dist';
-import { catchError, forkJoin, of, map } from 'rxjs';
+import { catchError, forkJoin, of, map, filter } from 'rxjs';
 import { FlightsSlice } from './interfaces';
+import { createHash } from 'node:crypto'
 
 
 @Injectable()
@@ -16,8 +17,18 @@ export class FlightsService {
                 this.httpService.get<FlightsSlice[]>(source).pipe(catchError(e => of(e)))
             )
         ).pipe(map((results: any[]) => {
-            const objs =  {flights: results[0].data["flights"].concat(results[1].data["flights"])};
-            return objs;
+            const source = [...results[0].data?.flights, ...results[1].data?.flights];
+            let seen = {};
+
+            const mergedFlights = source.filter(flight => {
+                const hash = createHash('md5').update(JSON.stringify(flight.slices)).digest('hex');
+                if (!seen.hasOwnProperty(hash)) {
+                  seen[hash] = true
+                  return flight;
+                }
+            });
+            
+            return {mergedFlights}; 
         }));
 
         return flights;
