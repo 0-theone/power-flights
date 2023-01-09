c42dd01dd6c2483fa652fbe4280d36ec7cf87431
Raissa Hohenester <raissa.developer@gmail.com>
2023-01-09 13:41:40 +0100

    Add mock file

--- src/flights/flights.service.spec.ts
+++ src/flights/flights.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test } from '@nestjs/testing';
 import { FlightsService } from './flights.service';
 import { HttpService } from '@nestjs/axios/dist';
+import { ConfigService } from '@nestjs/config';
+import { duplicatedFlightsMock } from './flightsMock';
 
 describe('FlightsService', () => {
   let service: FlightsService;
@@ -12,12 +14,20 @@ describe('FlightsService', () => {
       get: () => Promise.resolve([]),
     };
 
+    const fakeConfigService = {
+      get: () => "",
+    };
+
     const module = await Test.createTestingModule({
       providers: [
         FlightsService,
         {
           provide: HttpService,
           useValue: fakeHttpService,
+        },
+        {
+          provide: ConfigService,
+          useValue: fakeConfigService,
         },
       ],
     }).compile();
@@ -30,51 +40,7 @@ describe('FlightsService', () => {
   });
 
   it('filters an array of duplicates', async () => {
-    const flights = [
-        {
-          "slices": [
-              {
-                "origin_name": "Schonefeld",
-                "destination_name": "Stansted",
-                "departure_date_time_utc": "2019-08-08T04:30:00.000Z",
-                "arrival_date_time_utc": "2019-08-08T06:25:00.000Z",
-                "flight_number": "144",
-                "duration": 115
-              },
-              {
-                "origin_name": "Stansted",
-                "destination_name": "Schonefeld",
-                "departure_date_time_utc": "2019-08-10T05:35:00.000Z",
-                "arrival_date_time_utc": "2019-08-10T07:35:00.000Z",
-                "flight_number": "8542",
-                "duration": 120
-              }
-          ],
-        "price": 129
-      },
-      {
-        "slices": [
-            {
-                "origin_name": "Schonefeld",
-                "destination_name": "Stansted",
-                "departure_date_time_utc": "2019-08-08T04:30:00.000Z",
-                "arrival_date_time_utc": "2019-08-08T06:25:00.000Z",
-                "flight_number": "144",
-                "duration": 115
-            },
-            {
-                "origin_name": "Stansted",
-                "destination_name": "Schonefeld",
-                "departure_date_time_utc": "2019-08-10T05:35:00.000Z",
-                "arrival_date_time_utc": "2019-08-10T07:35:00.000Z",
-                "flight_number": "8542",
-                "duration": 120
-            }
-        ],
-        "price": 129
-      },
-    ];
-    const filteredArray = service.removeDuplicates(flights);
+    const filteredArray = service.removeDuplicates(duplicatedFlightsMock);
     expect(filteredArray.length).toEqual(1);
   });
  
